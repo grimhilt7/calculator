69b40679d76508be247f1a1a72cb679f7bde898d
grimhilt7 <116620023+grimhilt7@users.noreply.github.com>
2023-01-09 15:54:05 +0900

    Add keyboard feature

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function adjustFontsize(value) {
   }
 }
 
-const calculater = (() => {
+const calculator = (() => {
   const sum = (a, b) => a + b;
   const sub = (a, b) => a - b;
   const mul = (a, b) => a * b;
@@ -35,7 +35,7 @@ const calculater = (() => {
 
 
 function operate(numOne, numTwo, selection) {
-  return calculater[selection](numOne, numTwo);
+  return calculator[selection](numOne, numTwo);
 }
 
 const tasker = (() => {
@@ -75,11 +75,11 @@ function updateDisplay(value) {
   result.textContent = value;
 }
 
-function inputNumber() {
+function inputNumber(value) {
   if (isDone) { // begin a new start if the calculation has been completed
     initialize();
   }
-  number += this.id.slice(-1);
+  number += value;
   if (number === '0') { // prevent meaningless zeros
     number = '';
   }
@@ -94,19 +94,48 @@ function inputPeriod() {
   updateDisplay(number);
 }
 
-function inputOperator() {
-  operator = this.id;
+function inputOperator(value) {
+  operator = value;
   temp = number;
   number = '';
 
-  indicator.textContent = this.id;
+  indicator.textContent = value;
 }
 
-function inputFunction() {
-  tasker[this.id]();
+function inputFunction(value) {
+  tasker[value]();
 }
 
-numberButtons.forEach(numberButton => numberButton.addEventListener('click', inputNumber));
-operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click', inputOperator));
-functionButtons.forEach(functionButton => functionButton.addEventListener('click', inputFunction));
+function inputKeyboard(key) {
+  console.log(key);
+  if (['1','2','3','4','5','6','7','8','9','0'].includes(key)) {
+    inputNumber(key);
+  } else if (['+', '-', '*', '/'].includes(key)) {
+    switch (key) {
+      case '+':
+        inputOperator('sum');
+        break;
+      case '-':
+        inputOperator('sub');
+        break;
+      case '*':
+        inputOperator('mul');
+        break;
+      case '/':
+        inputOperator('div');
+        break
+      default:
+        
+    }
+  } else if (['Enter', 'Backspace'].includes(key)) {
+    inputFunction(key === 'Enter' ? 'equal' : 'backspace');
+  } else if (key === '.') {
+    inputPeriod();
+  }
+}
+
+numberButtons.forEach(numberButton => numberButton.addEventListener('click', function() {inputNumber(this.id.slice(-1));}));
+operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click', function() {inputOperator(this.id);}));
+functionButtons.forEach(functionButton => functionButton.addEventListener('click', function() {inputFunction(this.id);}));
 periodButton.addEventListener('click', inputPeriod);
+document.addEventListener('keydown', e => inputKeyboard(e.key)); // enable keyboard input
